be7209076becb0ec576c7057f85d0fbdaf4eac0a
aravedev <dravedev@gmail.com>
2022-09-11 20:02:33 +0200

    Added description each hero in main page

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { getHeroesByPublisher } from "../helpers/getHeroesByPublisher";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
   //console.log(dcHero);
   return (
-    <ul>
+    <div className="row row-cols-1 row-cols-md-3 g-3">
       {heroes.map((heroe) => (
-        <li key={heroe.id}>{heroe.superhero}</li>
+        <HeroCard key={heroe.id} {...heroe}></HeroCard>
       ))}
-    </ul>
+    </div>
   );
 };
+
+//
+
+//<li key={heroe.id}>{heroe.superhero}</li>
