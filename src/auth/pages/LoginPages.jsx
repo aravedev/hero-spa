6f3eb0a9ccc3cb5bac3de35a1c5668009fa2f813
aravedev <dravedev@gmail.com>
2022-09-11 18:36:36 +0200

    Added Login and LogOut functions in the Navbar

--- src/auth/pages/LoginPages.jsx
+++ src/auth/pages/LoginPages.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const LoginPages = () => {
-  return <h1>LoginPages</h1>;
+  const login = useNavigate();
+
+  const onLogin = () => {
+    login("marvel", { replace: true });
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>LoginPages</h1>
+        <hr />
+        <button className="btn btn-primary" onClick={onLogin}>
+          LogIn
+        </button>
+      </div>
+    </>
+  );
 };
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,11 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+  const onLogout = () => {
+    navigate("/login", { replace: true });
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -50,7 +55,10 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end ">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-info">Danny Rave</span>
-          <button className="nav-item nav-link btn btn btn-primary">
+          <button
+            className="nav-item nav-link btn btn btn-primary"
+            onClick={onLogout}
+          >
             LogOut
           </button>
         </ul>
